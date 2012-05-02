f98dc84406b5aa250d9975467c4f82a7bcd87671
Drew Young <drewyoung1@gmail.com>
2012-05-02 16:09:15 -0400

    Add support for users

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 //define(['require', 'exports', 'lib/index.js'], function(require, exports, os) {
-require(['lib/index', 'js/test.js'], function(os, test) {
+require(['lib/index', 'js/test', 'lib/user'], function(os, test, user) {
 	window.os = os;
+	window.Machine = os.Machine;
 	window.test = test;
+	window.User = user.User;
 });
 //});
